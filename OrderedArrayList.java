fd9dd05640c22a883331ec504700ef2a85464690
Derek Lao <dlao00@stuy.edu>
2018-11-05 03:04:31 -0500

    code worked with both Vincent's Driver and Timothy's driver

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,40 +8,24 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public boolean add(T element){
-    for(int i=0;i<size();i++)
-    {
-      if(i==0 && (element.compareTo(get(i))<=0))
-      {
-        super.add(i,element);
-        break;
-      }
-      if((element.compareTo(get(i))<=0) && (element.compareTo(get(i-1))>=0))
-      {
-        super.add(i,element);
-        break;
-      }
-      else
-      {
-        super.add(element);
-        break;
-      }
-    }
     if(size()==0)
     {
-      super.add(element);
+      return super.add(element);
     }
-    return true;
+    for(int i=0;i<size();i++)
+    {
+      if(element.compareTo(get(i))<=0)
+      {
+        super.add(i,element);
+        return true;
+      }
+      continue;
+    }
+    return super.add(element);
   }
 
   public void add(int index,T element){
-    if((index==0||get(index-1).compareTo(element)<=0)&&(index==size()||get(index+1).compareTo(element)>=0))
-    {
-      super.add(index,element);
-    }
-    else
-    {
-      super.add(element);
-    }
+    add(element);
   }
 
   public T set(int index,T element){
